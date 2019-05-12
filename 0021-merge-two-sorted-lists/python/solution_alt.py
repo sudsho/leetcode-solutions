997abe3a6ee67b5a8b77f1ce2234e08869d197c9
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-05-13 00:29:42 +0530

    alt solution for 21

---
+++ 0021-merge-two-sorted-lists/python/solution_alt.py
@@ -0,0 +1,15 @@
+# alt approach: recursive merge. concise, but uses O(n+m) call stack
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        # recursive version - shorter but uses stack space
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
