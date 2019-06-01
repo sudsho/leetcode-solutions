77fc767e639e0a041947ed3a7abd4dda6fc4c3d2
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-06-01 22:40:25 +0530

    cleanup

---
+++ 0067-add-binary/README.md
@@ -0,0 +1,20 @@
+# 67. Add Binary
+
+Difficulty: Easy
+Topics: string, math
+
+## Problem
+
+Given two binary strings a and b, return their sum as a binary string.
+
+## Approach
+
+Walk both strings from the right with a carry. Build the result digit by digit, then reverse it.
+
+## Complexity
+
+Time O(max(n,m)), space O(max(n,m)).
+
+## Files
+
+- `python/solution.py`
---
+++ 0067-add-binary/python/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addBinary(self, a, b):
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+        out = []
+        while i >= 0 or j >= 0 or carry:
+            x = int(a[i]) if i >= 0 else 0
+            y = int(b[j]) if j >= 0 else 0
+            s = x + y + carry
+            carry = s // 2
+            out.append(str(s % 2))
+            i -= 1
+            j -= 1
+        return ''.join(reversed(out))
