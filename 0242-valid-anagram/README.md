24b0b26025896900523dde1b2389064d4499d177
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-10-09 12:13:42 +0530

    easy: 242 valid anagram

---
+++ 0242-valid-anagram/README.md
@@ -0,0 +1,20 @@
+# 242. Valid Anagram
+
+Difficulty: Easy
+Topics: string, hash table
+
+## Problem
+
+Decide whether two strings are anagrams of each other (same letters, same counts).
+
+## Approach
+
+Count letters of one string, decrement for the other. If all counts end at zero, anagram. Or just compare sorted strings.
+
+## Complexity
+
+Time O(n), space O(1) (alphabet size).
+
+## Files
+
+- `python/solution.py`
---
+++ 0242-valid-anagram/python/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        # letter counts
+        counts = {}
+        for c in s:
+            counts[c] = counts.get(c, 0) + 1
+        for c in t:
+            if c not in counts:
+                return False
+            counts[c] -= 1
+            if counts[c] < 0:
+                return False
+        return True
