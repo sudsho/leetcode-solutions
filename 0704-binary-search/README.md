ad0cb7e567977dfdd5bc27988599bb4321173fd5
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-11-13 20:39:58 +0530

    refactor 704

---
+++ 0704-binary-search/README.md
@@ -0,0 +1,20 @@
+# 704. Binary Search
+
+Difficulty: Easy
+Topics: binary search, array
+
+## Problem
+
+Search a sorted array for a target. Return its index, or -1 if missing.
+
+## Approach
+
+Standard binary search with lo and hi pointers.
+
+## Complexity
+
+Time O(log n), space O(1).
+
+## Files
+
+- `python/solution.py`
---
+++ 0704-binary-search/python/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums, target):
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return -1
