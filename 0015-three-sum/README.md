4b0fc85d79dacdd7f9b11ec00931f565f9ab5bf3
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-12-31 20:12:37 +0530

    clean up 15

---
+++ 0015-three-sum/README.md
@@ -0,0 +1,20 @@
+# 15. 3Sum
+
+Difficulty: Medium
+Topics: array, two pointers
+
+## Problem
+
+Given nums, find all unique triplets a, b, c such that a + b + c == 0. The result must not contain duplicate triplets.
+
+## Approach
+
+Sort. For each i, two-pointer scan over the rest looking for sum to -nums[i]. Skip duplicates at every level.
+
+## Complexity
+
+Time O(n^2), space O(1) extra (output not counted).
+
+## Files
+
+- `python/solution.py`
---
+++ 0015-three-sum/python/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        out = []
+        n = len(nums)
+        for i in range(n - 2):
+            if nums[i] > 0:
+                break  # sorted, cannot sum to 0 anymore
+            if i > 0 and nums[i] == nums[i-1]:
+                continue  # skip dup
+            l, r = i + 1, n - 1
+            target = -nums[i]
+            while l < r:
+                s = nums[l] + nums[r]
+                if s == target:
+                    out.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+                elif s < target:
+                    l += 1
+                else:
+                    r -= 1
+        return out
