ae68f1339cbc42508cf669c285f6d00092d2b507
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-02-07 22:29:04 +0530

    another way for 26

---
+++ 0026-remove-duplicates-from-sorted-array/python/solution_alt.py
@@ -0,0 +1,11 @@
+# write-pointer style, slightly tighter
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
