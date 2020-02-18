66767813c1f7ab35bd717ca43a5e7e17fb98dd79
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-02-18 23:09:32 +0530

    alt 300

---
+++ 0300-longest-increasing-subsequence/python/solution_alt.py
@@ -0,0 +1,12 @@
+# n^2 dp - clearer
+class Solution:
+    def lengthOfLIS(self, nums):
+        if not nums:
+            return 0
+        n = len(nums)
+        dp = [1] * n
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] < nums[i] and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+        return max(dp)
