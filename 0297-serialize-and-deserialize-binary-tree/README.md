5104df2f0b6b511e2e7881b198539b82b48f3940
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-03-03 19:17:10 +0530

    small refactor

---
+++ 0297-serialize-and-deserialize-binary-tree/README.md
@@ -0,0 +1,20 @@
+# 297. Serialize and Deserialize Binary Tree
+
+Difficulty: Hard
+Topics  : tree, design, dfs, bfs
+
+## Problem
+
+Design serialize and deserialize for a binary tree.
+
+## Approach
+
+Preorder with explicit None markers. Encode as a comma-joined string.
+
+## Complexity
+
+Time O(n), space O(n).
+
+## Files
+
+- `python/solution.py`
---
+++ 0297-serialize-and-deserialize-binary-tree/python/solution.py
@@ -0,0 +1,26 @@
+class Codec:
+    def serialize(self, root):
+        out = []
+        def go(node):
+            if not node:
+                out.append("#")
+                return
+            out.append(str(node.val))
+            go(node.left)
+            go(node.right)
+        go(root)
+        return ",".join(out)
+
+    def deserialize(self, data):
+        tokens = iter(data.split(","))
+
+        def build():
+            t = next(tokens)
+            if t == "#":
+                return None
+            node = TreeNode(int(t))
+            node.left = build()
+            node.right = build()
+            return node
+
+        return build()
