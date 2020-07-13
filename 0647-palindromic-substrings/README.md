5be5bd4761e954e080121dc86a6fe75d0815d601
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-07-13 23:30:58 +0530

    add 647

---
+++ 0647-palindromic-substrings/README.md
@@ -0,0 +1,20 @@
+# 647. Palindromic Substrings
+
+Difficulty: Medium
+Topics  : string, dynamic programming
+
+## Problem
+
+Return the number of palindromic substrings in s.
+
+## Approach
+
+Expand around each center (2n-1 of them).
+
+## Complexity
+
+Time O(n^2), space O(1).
+
+## Files
+
+- `python/solution.py`
---
+++ 0647-palindromic-substrings/python/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countSubstrings(self, s):
+        count = 0
+        for i in range(len(s)):
+            count += self._expand(s, i, i)
+            count += self._expand(s, i, i + 1)
+        return count
+
+    def _expand(self, s, l, r):
+        c = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            c += 1
+            l -= 1
+            r += 1
+        return c
