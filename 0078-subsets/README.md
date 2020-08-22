ddcd048e3d053337332836e8b1d03bde04e8ea87
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-08-22 12:10:13 +0530

    save

---
+++ 0078-subsets/README.md
@@ -0,0 +1,20 @@
+# 78. Subsets
+
+Difficulty: Medium
+Topics  : array, backtracking, bit manipulation
+
+## Problem
+
+Given a list of unique ints return all possible subsets (the power set).
+
+## Approach
+
+Backtracking. At each index either include or skip.
+
+## Complexity
+
+Time O(2^n * n), space O(n).
+
+## Files
+
+- `python/solution.py`
---
+++ 0078-subsets/python/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums):
+        out = []
+        def go(i, path):
+            if i == len(nums):
+                out.append(path[:])
+                return
+            # skip
+            go(i + 1, path)
+            # include
+            path.append(nums[i])
+            go(i + 1, path)
+            path.pop()
+        go(0, [])
+        return out
