a1e3627fd0dbd447ea4ca56bbe2ceccc0ff830e9
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-11-22 22:35:43 +0530

    fix

---
+++ 0437-path-sum-iii/README.md
@@ -0,0 +1,20 @@
+# 437. Path Sum III
+
+Difficulty: Medium
+Topics  : tree, dfs, hash table, prefix sum
+
+## Problem
+
+Given a binary tree and an integer target return the number of paths whose values sum to target. Paths must go top-down but can start and end anywhere.
+
+## Approach
+
+DFS with a running prefix-sum count. Number of valid paths ending at the current node is the count of (prefix - target).
+
+## Complexity
+
+Time O(n), space O(n).
+
+## Files
+
+- `python/solution.py`
---
+++ 0437-path-sum-iii/python/solution.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class Solution:
+    def pathSum(self, root, targetSum):
+        prefix = defaultdict(int)
+        prefix[0] = 1
+        self.count = 0
+
+        def dfs(node, cur):
+            if not node:
+                return
+            cur += node.val
+            self.count += prefix[cur - targetSum]
+            prefix[cur] += 1
+            dfs(node.left, cur)
+            dfs(node.right, cur)
+            prefix[cur] -= 1
+
+        dfs(root, 0)
+        return self.count
