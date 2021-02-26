1d628747d930d127b635583c5f72827fe2889694
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-02-26 11:37:01 +0530

    add 84. largest rectangle in histogram

---
+++ 0084-largest-rectangle-in-histogram/README.md
@@ -0,0 +1,20 @@
+# 84. Largest Rectangle In Histogram
+
+Difficulty: Hard
+Topics  : stack, monotonic stack
+
+## Problem
+
+Largest rectangle area inside a histogram.
+
+## Approach
+
+Monotonic increasing stack of indices. On a smaller bar, pop and compute area with current i as right bound.
+
+## Complexity
+
+Time O(n), space O(n).
+
+## Files
+
+- `python/solution.py`
---
+++ 0084-largest-rectangle-in-histogram/python/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        st = []
+        best = 0
+        heights = heights + [0]
+        for i, h in enumerate(heights):
+            while st and heights[st[-1]] > h:
+                top = st.pop()
+                width = i if not st else i - st[-1] - 1
+                best = max(best, heights[top] * width)
+            st.append(i)
+        return best
