36f5caa863fe7c6a9dbe5263b90f2532dfb944c1
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-04-17 13:25:02 +0530

    leet 189

---
+++ 0189-rotate-array/README.md
@@ -0,0 +1,21 @@
+# 189. Rotate Array
+
+Difficulty: Medium
+Topics  : array, math
+
+## Problem
+
+Rotate array right by k steps in place.
+
+## Approach
+
+Reverse all, reverse first k, reverse rest. Three reversals trick.
+
+## Complexity
+
+Time O(n), space O(1).
+
+## Files
+
+- `python/solution.py`
+- `python/solution_alt.py`
---
+++ 0189-rotate-array/python/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+
+        def rev(i: int, j: int) -> None:
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1; j -= 1
+
+        rev(0, n - 1)
+        rev(0, k - 1)
+        rev(k, n - 1)
