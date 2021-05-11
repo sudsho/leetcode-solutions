bf52e70b04da0c37ed3abb50d83813ec2183c29b
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-05-11 18:46:15 +0530

    find median from data stream

---
+++ 0295-find-median-from-data-stream/README.md
@@ -0,0 +1,20 @@
+# 295. Find Median From Data Stream
+
+Difficulty: Hard
+Topics  : heap, design
+
+## Problem
+
+Add numbers and report the running median.
+
+## Approach
+
+Two heaps: max-heap of lower half, min-heap of upper half. Rebalance after each add.
+
+## Complexity
+
+Time O(log n) per add, O(1) for median.
+
+## Files
+
+- `python/solution.py`
---
+++ 0295-find-median-from-data-stream/python/solution.py
@@ -0,0 +1,17 @@
+import heapq
+
+class MedianFinder:
+    def __init__(self) -> None:
+        self.lo: list = []  # max-heap (negated)
+        self.hi: list = []  # min-heap
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.lo, -num)
+        heapq.heappush(self.hi, -heapq.heappop(self.lo))
+        if len(self.hi) > len(self.lo):
+            heapq.heappush(self.lo, -heapq.heappop(self.hi))
+
+    def findMedian(self) -> float:
+        if len(self.lo) > len(self.hi):
+            return float(-self.lo[0])
+        return (-self.lo[0] + self.hi[0]) / 2
