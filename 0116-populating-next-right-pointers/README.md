592724cffa8ea05014369160adaf197753e7e59c
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-10-12 19:26:53 +0530

    save

---
+++ 0116-populating-next-right-pointers/README.md
@@ -0,0 +1,20 @@
+# 116. Populating Next Right Pointers
+
+Difficulty: Medium
+Topics  : tree, bfs, linked list
+
+## Problem
+
+Connect each node to its next right neighbor in a perfect binary tree.
+
+## Approach
+
+Level-by-level using already-set next pointers, O(1) extra space.
+
+## Complexity
+
+Time O(n), space O(1).
+
+## Files
+
+- `python/solution.py`
---
+++ 0116-populating-next-right-pointers/python/solution.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: "Node") -> "Node":
+        head = root
+        while head and head.left:
+            cur = head
+            while cur:
+                cur.left.next = cur.right
+                if cur.next:
+                    cur.right.next = cur.next.left
+                cur = cur.next
+            head = head.left
+        return root
