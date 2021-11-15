94b06ea62f42b9e48a552c8097ba44059fc68027
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-11-15 11:40:05 +0530

    lowest common ancestor of binary tree

---
+++ 0236-lowest-common-ancestor-of-binary-tree/README.md
@@ -0,0 +1,20 @@
+# 236. Lowest Common Ancestor Of Binary Tree
+
+Difficulty: Medium
+Topics  : tree, dfs
+
+## Problem
+
+LCA in a generic binary tree.
+
+## Approach
+
+DFS. If both subtrees return non-null then current is LCA.
+
+## Complexity
+
+Time O(n), space O(h).
+
+## Files
+
+- `python/solution.py`
---
+++ 0236-lowest-common-ancestor-of-binary-tree/python/solution.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or root is p or root is q:
+            return root
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+        if l and r:
+            return root
+        return l or r
