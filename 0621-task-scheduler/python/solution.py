0cece7306cfbfe73898d747bdd2d9f69ccef4754
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-11-15 13:47:05 +0530

    tweak

--- 0621-task-scheduler/python/solution.py
+++ 0621-task-scheduler/python/solution.py
@@ -7,3 +7,4 @@ class Solution:
         max_freq = max(cnt.values())
         ties = sum(1 for v in cnt.values() if v == max_freq)
         return max(len(tasks), (max_freq - 1) * (n + 1) + ties)
+# typing fix
