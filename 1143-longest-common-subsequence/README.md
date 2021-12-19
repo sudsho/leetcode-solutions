7eefa192e9065655fdb4a34adf72c292f6dafddf
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-12-19 22:05:38 +0530

    leet 1143

---
+++ 1143-longest-common-subsequence/README.md
@@ -0,0 +1,20 @@
+# 1143. Longest Common Subsequence
+
+Difficulty: Medium
+Topics  : string, dp
+
+## Problem
+
+LCS of two strings.
+
+## Approach
+
+2D DP. Match increases, mismatch takes max of two neighbors.
+
+## Complexity
+
+Time O(mn), space O(min(m,n)).
+
+## Files
+
+- `python/solution.py`
---
+++ 1143-longest-common-subsequence/python/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonSubsequence(self, t1: str, t2: str) -> int:
+        if len(t1) < len(t2):
+            t1, t2 = t2, t1
+        prev = [0] * (len(t2) + 1)
+        for c1 in t1:
+            cur = [0] * (len(t2) + 1)
+            for j, c2 in enumerate(t2, 1):
+                if c1 == c2:
+                    cur[j] = prev[j - 1] + 1
+                else:
+                    cur[j] = max(prev[j], cur[j - 1])
+            prev = cur
+        return prev[-1]
