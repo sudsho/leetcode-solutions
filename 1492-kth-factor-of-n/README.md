ec91d2c3517527a246768dc33aa650cbc04dbee6
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-12-28 20:22:45 +0530

    added problem

---
+++ 1492-kth-factor-of-n/README.md
@@ -0,0 +1,20 @@
+# 1492. Kth Factor Of N
+
+Difficulty: Medium
+Topics  : math
+
+## Problem
+
+Kth smallest divisor of n, or -1.
+
+## Approach
+
+Iterate up to sqrt collecting divisors, then iterate complements.
+
+## Complexity
+
+Time O(sqrt(n)), space O(sqrt(n)).
+
+## Files
+
+- `python/solution.py`
---
+++ 1492-kth-factor-of-n/python/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        small = []
+        large = []
+        i = 1
+        while i * i <= n:
+            if n % i == 0:
+                small.append(i)
+                if i != n // i:
+                    large.append(n // i)
+            i += 1
+        factors = small + large[::-1]
+        return factors[k - 1] if k <= len(factors) else -1
