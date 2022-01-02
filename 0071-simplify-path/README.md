d1a247d2ef854a3299ee249be5b7185b4b71b9fc
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-01-03 00:13:38 +0530

    leetcode 71 done

---
+++ 0071-simplify-path/README.md
@@ -0,0 +1,20 @@
+# 71. Simplify Path
+
+Difficulty: Medium
+Topics  : stack, string
+
+## Problem
+
+Given a Unix-style absolute path, simplify it to its canonical form.
+
+## Approach
+
+Split on `/`. Stack: push valid name, pop on `..`, ignore `.` and empty.
+
+## Complexity
+
+Time O(N), space O(N).
+
+## Files
+
+- `python/solution.py`
---
+++ 0071-simplify-path/python/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack: List[str] = []
+        for part in path.split("/"):
+            if part == "" or part == ".":
+                continue
+            if part == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(part)
+        return "/" + "/".join(stack)
