5d8b7bab55bcbf129bbece60d0a4c94e42e85dc3
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-01-14 00:21:11 +0530

    alt approach for 71

---
+++ 0071-simplify-path/python/solution_alt.py
@@ -0,0 +1,15 @@
+# regex split + filter, more pythonic
+import re
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        for part in re.split(r"/+", path):
+            if part in ("", "."):
+                continue
+            if part == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(part)
+        return "/" + "/".join(stack)
