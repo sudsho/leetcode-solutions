0fb6750153bfa7ea39527da00cfcf74d99928442
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-03-19 00:12:01 +0530

    alt approach 354

---
+++ 0354-russian-doll-envelopes/python/solution_alt.py
@@ -0,0 +1,15 @@
+# straight n^2 dp - simpler though slower
+from typing import List
+
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        n = len(envelopes)
+        if n == 0:
+            return 0
+        dp = [1] * n
+        for i in range(1, n):
+            for j in range(i):
+                if envelopes[j][1] < envelopes[i][1]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
