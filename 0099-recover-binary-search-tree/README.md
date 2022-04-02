35a46f5ff29cadd63d9fa070724cd3c89a0965f6
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-04-02 19:08:47 +0530

    easy solution for 99

---
+++ 0099-recover-binary-search-tree/README.md
@@ -0,0 +1,20 @@
+# 99. Recover Binary Search Tree
+
+Difficulty: Medium
+Topics  : tree, inorder
+
+## Problem
+
+Two nodes of a BST were swapped by mistake. Recover the tree without altering its structure.
+
+## Approach
+
+Inorder traversal yields a sequence with two violations (or one adjacent). Track first/second offending nodes, swap their values.
+
+## Complexity
+
+Time O(N), space O(H).
+
+## Files
+
+- `python/solution.py`
---
+++ 0099-recover-binary-search-tree/python/solution.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val: int = 0, left: Optional["TreeNode"] = None, right: Optional["TreeNode"] = None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        first = second = prev = None
+        stack = []
+        node = root
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            if prev and prev.val > node.val:
+                if first is None:
+                    first = prev
+                second = node
+            prev = node
+            node = node.right
+        if first and second:
+            first.val, second.val = second.val, first.val
