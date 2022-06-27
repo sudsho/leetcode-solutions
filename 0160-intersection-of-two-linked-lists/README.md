8fa3746ee84fa76bedad70e7019204416288965c
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-06-27 23:01:05 +0530

    wip on 160

---
+++ 0160-intersection-of-two-linked-lists/README.md
@@ -0,0 +1,20 @@
+# 160. Intersection of Two Linked Lists
+
+Difficulty: Easy
+Topics  : linked list, two pointers
+
+## Problem
+
+Find the node where two singly linked lists intersect, or None.
+
+## Approach
+
+Two pointers walk a then b and b then a; they meet at intersection or both reach None.
+
+## Complexity
+
+Time O(N+M), space O(1).
+
+## Files
+
+- `python/solution.py`
---
+++ 0160-intersection-of-two-linked-lists/python/solution.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val: int = 0, next: Optional["ListNode"] = None) -> None:
+        self.val = val
+        self.next = next
+
+class Solution:
+    def getIntersectionNode(self, headA: Optional[ListNode], headB: Optional[ListNode]) -> Optional[ListNode]:
+        a, b = headA, headB
+        while a is not b:
+            a = a.next if a else headB
+            b = b.next if b else headA
+        return a
