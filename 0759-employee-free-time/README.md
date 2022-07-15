c08f73f7e52ff2f1beca27c30654ab0198987275
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-07-15 21:56:38 +0530

    wip on 759

---
+++ 0759-employee-free-time/README.md
@@ -0,0 +1,20 @@
+# 759. Employee Free Time
+
+Difficulty: Hard
+Topics  : heap, sweep
+
+## Problem
+
+Given each employee's working intervals, return the free intervals shared by all.
+
+## Approach
+
+Min heap on (start, employee, idx). Track current max end. New gap when next start > current end.
+
+## Complexity
+
+Time O(N log K), space O(K).
+
+## Files
+
+- `python/solution.py`
---
+++ 0759-employee-free-time/python/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+import heapq
+
+class Interval:
+    def __init__(self, start: int = 0, end: int = 0) -> None:
+        self.start = start
+        self.end = end
+
+class Solution:
+    def employeeFreeTime(self, schedule: List[List[Interval]]) -> List[Interval]:
+        heap = []
+        for ei, ints in enumerate(schedule):
+            heapq.heappush(heap, (ints[0].start, ei, 0))
+        result: List[Interval] = []
+        prev_end = heap[0][0]
+        while heap:
+            start, ei, j = heapq.heappop(heap)
+            if start > prev_end:
+                result.append(Interval(prev_end, start))
+            prev_end = max(prev_end, schedule[ei][j].end)
+            if j + 1 < len(schedule[ei]):
+                heapq.heappush(heap, (schedule[ei][j + 1].start, ei, j + 1))
+        return result
