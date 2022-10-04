a536b8f9161e519008dc395f366f1eb5a2a249ff
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-10-04 21:08:33 +0530

    alt solve for 315

---
+++ 0315-count-of-smaller-numbers-after-self/python/solution_alt.py
@@ -0,0 +1,28 @@
+# segment tree on value rank
+from typing import List
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        sorted_unique = sorted(set(nums))
+        rank = {v: i for i, v in enumerate(sorted_unique)}
+        tree = [0] * (len(sorted_unique) + 1)
+
+        def update(i: int) -> None:
+            i += 1
+            while i < len(tree):
+                tree[i] += 1
+                i += i & -i
+
+        def query(i: int) -> int:
+            s = 0
+            while i > 0:
+                s += tree[i]
+                i -= i & -i
+            return s
+
+        result = [0] * len(nums)
+        for k in range(len(nums) - 1, -1, -1):
+            r = rank[nums[k]]
+            result[k] = query(r)
+            update(r)
+        return result
