c1378d29fed1ba589af00c2ae498b61a32f2fd25
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-12-13 21:38:50 +0530

    got 158 working

---
+++ 0158-read-n-characters-given-read4-ii/README.md
@@ -0,0 +1,20 @@
+# 158. Read N Characters Given Read4 II
+
+Difficulty: Hard
+Topics  : design
+
+## Problem
+
+Wrap read4 to support multiple read calls. Buffer leftover chars between calls.
+
+## Approach
+
+Maintain a small internal buffer of size 4 plus pointers for head and tail.
+
+## Complexity
+
+Time O(N), space O(1).
+
+## Files
+
+- `python/solution.py`
---
+++ 0158-read-n-characters-given-read4-ii/python/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+def read4(buf4: List[str]) -> int: ...
+
+class Solution:
+    def __init__(self) -> None:
+        self.buffer = [""] * 4
+        self.head = 0
+        self.tail = 0
+
+    def read(self, buf: List[str], n: int) -> int:
+        i = 0
+        while i < n:
+            if self.head == self.tail:
+                self.tail = read4(self.buffer)
+                self.head = 0
+                if self.tail == 0:
+                    break
+            while i < n and self.head < self.tail:
+                buf[i] = self.buffer[self.head]
+                i += 1
+                self.head += 1
+        return i
