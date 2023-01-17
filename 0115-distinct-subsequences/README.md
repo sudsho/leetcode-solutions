ebf7ebdd721a878360d4de442e285c0baaa54dbc
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2023-01-17 22:49:41 +0530

    leetcode 115 done

---
+++ 0115-distinct-subsequences/README.md
@@ -0,0 +1,12 @@
+# 115. Distinct Subsequences
+
+Difficulty: Hard
+Topics  : dp, strings
+
+## Problem
+
+Count distinct subsequences of s equal to t. Classic 2D dp on (i, j).
+
+## Files
+
+- `python/solution.py`
---
+++ 0115-distinct-subsequences/python/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        if n > m:
+            return 0
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for ch in s:
+            for j in range(n, 0, -1):
+                if ch == t[j - 1]:
+                    dp[j] += dp[j - 1]
+        return dp[n]
