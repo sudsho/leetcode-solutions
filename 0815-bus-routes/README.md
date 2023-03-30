b2419d9bee88c57030fbf2e5c7a154f6880408fe
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2023-03-30 20:54:42 +0530

    alt for 815, 2023 pass

--- 0815-bus-routes/README.md
+++ 0815-bus-routes/README.md
@@ -18,3 +18,4 @@ Time O(sum of route sizes), space O(sum of route sizes).
 ## Files
 
 - `python/solution.py`
+- `python/solution_alt.py`
---
+++ 0815-bus-routes/python/solution_alt.py
@@ -0,0 +1,32 @@
+# alt: revisited in 2023, slight cleanup
+from typing import List, Optional
+
+# original idea kept; this version uses match/dataclass-style refactor where it helps.
+
+from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source == target:
+            return 0
+        stop_to_routes = defaultdict(set)
+        for i, r in enumerate(routes):
+            for s in r:
+                stop_to_routes[s].add(i)
+        visited_routes = set()
+        visited_stops = {source}
+        q = deque([(source, 0)])
+        while q:
+            stop, buses = q.popleft()
+            for ri in stop_to_routes[stop]:
+                if ri in visited_routes:
+                    continue
+                visited_routes.add(ri)
+                for s in routes[ri]:
+                    if s == target:
+                        return buses + 1
+                    if s not in visited_stops:
+                        visited_stops.add(s)
+                        q.append((s, buses + 1))
+        return -1
