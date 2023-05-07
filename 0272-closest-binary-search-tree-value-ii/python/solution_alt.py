2a21ac4aa1c92ca566c49267c5042b5b93d97668
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2023-05-07 18:24:50 +0530

    alt for 272, 2023 pass

---
+++ 0272-closest-binary-search-tree-value-ii/python/solution_alt.py
@@ -0,0 +1,35 @@
+# alt: revisited in 2023, slight cleanup
+from typing import List, Optional
+
+# original idea kept; this version uses match/dataclass-style refactor where it helps.
+
+from typing import List, Optional
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val: int = 0, left: Optional["TreeNode"] = None, right: Optional["TreeNode"] = None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
+        out = deque()
+        stack = []
+        node = root
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            if len(out) < k:
+                out.append(node.val)
+            else:
+                if abs(node.val - target) < abs(out[0] - target):
+                    out.popleft()
+                    out.append(node.val)
+                else:
+                    break
+            node = node.right
+        return list(out)
+# follow up: revisit if profiling cares
