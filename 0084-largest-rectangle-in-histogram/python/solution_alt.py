41877beba10c660e6e9627e85b856d5730d2a92a
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2023-11-17 20:42:19 +0530

    0084-largest-rectangle-in-histogram: tidied

---
+++ 0084-largest-rectangle-in-histogram/python/solution_alt.py
@@ -0,0 +1,20 @@
+# alt: revisited in 2023, slight cleanup
+from typing import List, Optional
+
+# original idea kept; this version uses match/dataclass-style refactor where it helps.
+
+from typing import List
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        st = []
+        best = 0
+        heights = heights + [0]
+        for i, h in enumerate(heights):
+            while st and heights[st[-1]] > h:
+                top = st.pop()
+                width = i if not st else i - st[-1] - 1
+                best = max(best, heights[top] * width)
+            st.append(i)
+        return best
+# typing fix
