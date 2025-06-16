3589ae5bbbef8c743a120d5552d3de14aa03c878
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2025-06-17 00:33:17 +0530

    3244 solution

---
+++ 3244-shortest-distance-after-road-addition-queries-ii/README.md
@@ -0,0 +1,12 @@
+# 3244. Shortest Distance After Road Addition Queries II
+
+Difficulty: Hard
+Topics  : sorted intervals
+
+## Problem
+
+Maintain merged intervals as edges are added.
+
+## Files
+
+- `python/solution.py`
---
+++ 3244-shortest-distance-after-road-addition-queries-ii/python/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+from sortedcontainers import SortedList
+
+class Solution:
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        # represent active "next" pointers using a sorted list of nodes still in the chain
+        sl = SortedList(range(n))
+        ans: list[int] = []
+        for u, v in queries:
+            i = sl.bisect_right(u)
+            # remove nodes strictly between u and v
+            while i < len(sl) and sl[i] < v:
+                sl.pop(i)
+            ans.append(len(sl) - 1)
+        return ans
