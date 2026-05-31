4c5a3b7a42f3c8c62f1221ff1730805e5ff0b0c4
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2026-05-30 20:52:32 -0400

    add docstring to reverse string two pointer helper

--- 0344-reverse-string/python/solution.py
+++ 0344-reverse-string/python/solution.py
@@ -1,6 +1,6 @@
-# revisited
 class Solution:
     def reverseString(self, s):
+        """Reverse the character list in place with two pointers from both ends."""
         i, j = 0, len(s) - 1
         while i < j:
             s[i], s[j] = s[j], s[i]
