8f057d0c25ff3a98d27995ba6bdb3058f4f97955
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2026-06-02 22:14:10 -0400

    tidy comment on rotate image, transpose+reverse is the why

--- 0048-rotate-image/python/solution.py
+++ 0048-rotate-image/python/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def rotate(self, matrix):
+        # 90 deg clockwise = transpose then reverse each row
         n = len(matrix)
-        # transpose
         for i in range(n):
             for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        # reverse each row
         for row in matrix:
             row.reverse()
